c8125f2c270d82b4614b8c40e308dfe98da06e34
= <niccolo.dellarocca@gmail.com>
2026-04-25 13:58:43 +0200

    Fixed incompatible type for aria-label

--- src/components/Page/Display Case/Featured/Prominent News Grid/ProminentNewsGrid.tsx
+++ src/components/Page/Display Case/Featured/Prominent News Grid/ProminentNewsGrid.tsx
@@ -35,7 +35,7 @@ const ProminentNewsCell: React.FC = () => {
     return (
         <article style={{paddingBottom: "16px"}}>
             <img src={NewsPlaceholder} alt="featured news (0,0)"/>
-            <Text as="a" font={theme.textStyle.prominent} style={{ lineHeight: 1.1 }} aria-role="heading" aria-level="2">
+            <Text as="a" font={theme.textStyle.prominent} style={{ lineHeight: 1.1 }} aria-role="heading" aria-level={2}>
                 {lorem.generateSentences(1)}
             </Text>
         </article>
